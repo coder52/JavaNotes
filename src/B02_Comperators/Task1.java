97f996e3b6521183817588ff15b51d7e1a73f038
Sahin <sahinmuhsin2018@gmail.com>
2023-04-15 15:47:37 +0200

    Update Comparators

--- src/B02_Comperators/Task1.java
+++ src/B02_Comperators/Task1.java
@@ -72,8 +72,8 @@ class Task1 {
         scan.close();
 
         Arrays.sort(player, checker);
-        for(int i = 0; i < player.length; i++){
-            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        for (Player value : player) {
+            System.out.printf("%s %s\n", value.name, value.score);
         }
     }
 }
