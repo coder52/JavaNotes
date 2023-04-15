b4a70efca1bb745f2c853b1466a33cdaa2fa5f59
Sahin <sahinmuhsin2018@gmail.com>
2023-04-15 15:44:26 +0200

    Task for Priority Queue

--- src/B04_PriorityQueue/Solution.java
+++ src/B04_PriorityQueue/Solution.java
@@ -1,13 +1,40 @@
 package B04_PriorityQueue;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
+import java.util.*;
 
 
 /*
- * Create the Student and Priorities classes here.
+Bu alistirmada PriorityQueue Comparators ve Comparable ile beraber kullnaimina ornek veriyoruz.
+
+Bu alistirmada ENTER komutu gorulunce yeni kayit yapilmali ve SERVED komutu gorulunce CGPA'si en yuksek olan kisi
+listeden cikarilmali.
+Consola girilen inputun ilk sayisi kac giris yapilacagini gosterir. Daha sonra "ENTER name CGPA id" yada SERVED
+komutlari gorulecektir
+    Input:
+        12
+        ENTER John 3.75 50
+        ENTER Mark 3.8 24
+        ENTER Shafaet 3.7 35
+        SERVED
+        SERVED
+        ENTER Samiha 3.85 36
+        SERVED
+        ENTER Ashley 3.9 42
+        ENTER Maria 3.6 46
+        ENTER Anik 3.95 49
+        ENTER Dan 3.95 50
+        SERVED
+    Output:
+        Dan
+        Ashley
+        Shafaet
+        Maria
+
+ Dikkat ederseniz Student class Comparable interface'ini implement ediyor ve boylece Student ile olusturlacak
+ olan Queue, compareTo metodunun kurallarina gore otomatik siralanacak.
  */
-class Student {
+
+class Student implements Comparable<Student>{
 
     private int id;
     private String name;
@@ -21,19 +48,63 @@ class Student {
     public int getId() {
         return id;
     }
-
     public String getName() {
         return name;
     }
-
     public double getCgpa() {
         return cgpa;
+    }
+
+    public int compareTo(Student student) {
+        if(student.getCgpa()>this.cgpa){
+            return 1;
+        }else if(student.getCgpa()<this.cgpa){
+            return -1;
+        }else{
+            return name.compareTo(student.name);
+        }
+    }
+}
+
+class Checker implements Comparator{
+    public int compare(Object obj1,Object obj2){
+        Student s1=(Student) obj1;
+        Student s2=(Student) obj2;
+
+        if(s1.getCgpa() == s2.getCgpa())
+            return s1.getName().compareTo(s2.getName());
+        else if(s1.getCgpa() > s2.getCgpa())
+            return -1;
+        else
+            return 1;
     }
 }
 
 class Priorities {
     List<Student> getStudents(List<String> events){
-        return null;
+        Queue<Student> queue = new PriorityQueue<>();
+        Checker checker = new Checker();
+        for (int i = 0; i < events.size(); i++) {
+            Scanner input = new Scanner(events.get(i));
+            String command = input.next();
+            if (command.equals("ENTER")) {
+                String name = input.next();
+                Double cgpa = Double.parseDouble(input.next());
+                int id = input.nextInt();
+                Student student= new Student(id, name, cgpa);
+                queue.add(student);
+            } else if (command.equals("SERVED"))  {
+                if(!queue.isEmpty()){
+                    queue.remove();
+                }
+            }
+        }
+        List<Student> students = new ArrayList<Student>() ;
+        for (Student st:queue) {
+            students.add(st);
+        }
+        Collections.sort(students, checker);
+        return students;
     }
 }
 
@@ -56,7 +127,7 @@ public class Solution {
             System.out.println("EMPTY");
         } else {
             for (Student st: students) {
-                System.out.println(st.getName());
+                System.out.println(st.getName()+" "+ st.getCgpa()+" "+st.getId());
             }
         }
     }
