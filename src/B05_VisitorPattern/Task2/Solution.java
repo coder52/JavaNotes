6f0cae36c7e1904d2891161453d293651350a7dc
sahin <sahinmuhsin2018@gmail.com>
2023-05-19 15:36:32 +0200

    Visitors Task2

--- src/B05_VisitorPattern/Task2/Solution.java
+++ src/B05_VisitorPattern/Task2/Solution.java
@@ -1,6 +1,8 @@
 package B05_VisitorPattern.Task2;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 
 enum Color {
@@ -105,6 +107,9 @@ class ProductOfRedNodesVisitor extends TreeVis {
 
     public void visitLeaf(TreeLeaf leaf) {
         //implement this
+        if(leaf.getColor().equals(Color.RED)){
+            total*=leaf.getValue();
+        }
     }
 }
 
@@ -120,16 +125,16 @@ class FancyVisitor extends TreeVis {
         //implement this
         if(node.getDepth()==0){
             totalValuesOfNonLeafNodes+=node.getValue();
-            System.out.println(node.getValue());
         }
+//        System.out.println(node.getValue());
     }
 
     public void visitLeaf(TreeLeaf leaf) {
         //implement this
         if(leaf.getColor().equals(Color.GREEN)){
-            totalValuesOfGreenLeaf*=leaf.getValue();
-            System.out.println(leaf.getValue());
+            totalValuesOfGreenLeaf+=leaf.getValue();
         }
+        System.out.println(leaf.getValue());
     }
 }
 
@@ -143,22 +148,42 @@ public class Solution {
         String[] nodeValues = input.nextLine().split(" ");
         String[] nodeColors = input.nextLine().split(" ");
 
+        List<Integer> u_list = new ArrayList<>();
+        u_list.add(1);
+        List<Integer> v_list = new ArrayList<>();
+        v_list.add(1);
         int[] depths= new int[numberOfNodes];
         depths[0]=0;
-        TreeNode root = new TreeNode(Integer.valueOf(nodeValues[0]),
-                Color.values()[Integer.valueOf(nodeColors[0])],
-                0);
         for (int i = 1; i < numberOfNodes; i++) {
-            int nodeValue = Integer.valueOf(nodeValues[i]);
-            Color nodeColor = Color.values()[Integer.valueOf(nodeColors[i])];
             int u = input.nextInt();
+            u_list.add(u);
             int v = input.nextInt();
+            v_list.add(v);
             if(u==1){
                 depths[v-1] = 1;
             } else {
                 depths[v-1] = depths[u-1]+1;
             }
-            Tree node = new TreeNode(nodeValue,nodeColor,depths[v-1]);
+        }
+        HashSet<Integer> u_set = new HashSet<>(u_list);
+        System.out.println(u_set);
+        HashSet<Integer> v_set = new HashSet<>(v_list);
+        System.out.println(v_set);
+        v_set.removeAll(u_set);
+        System.out.println(v_set);
+
+
+        TreeNode root = new TreeNode(Integer.valueOf(nodeValues[0]),Color.values()[Integer.valueOf(nodeColors[0])],0);
+        for (int i = 1; i < numberOfNodes; i++) {
+            int nodeValue = Integer.valueOf(nodeValues[i]);
+            Color nodeColor = Color.values()[Integer.valueOf(nodeColors[i])];
+            Tree node;
+            if(u_set.contains(i)){
+                node = new TreeLeaf(nodeValue,nodeColor,depths[i]);
+            }
+            else {
+                node = new TreeNode(nodeValue,nodeColor,depths[i]);
+            }
             root.addChild(node);
         }
         return root;
