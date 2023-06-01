18f677a1e0ee1ca409c07e6f1c90ba90c8641e2f
Sahin <sahinmuhsin2018@gmail.com>
2023-06-01 23:02:45 +0200

    Annotations initial commit

---
+++ src/B06_Annotations/Solution.java
@@ -0,0 +1,67 @@
+package B06_Annotations;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+import java.util.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface FamilyBudget {
+    String userRole() default "GUEST";
+	/*TODO ~~Complete the interface~~*/
+}
+
+class FamilyMember {
+    /*TODO ~~Complete the interface~~*/
+    public void seniorMember(int budget, int moneySpend) {
+        System.out.println("Senior Member");
+        System.out.println("Spend: " + moneySpend);
+        System.out.println("Budget Left: " + (budget - moneySpend));
+    }
+
+    /*TODO ~~Complete the interface~~*/
+    public void juniorUser(int budget, int moneySpend) {
+        System.out.println("Junior Member");
+        System.out.println("Spend: " + moneySpend);
+        System.out.println("Budget Left: " + (budget - moneySpend));
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String role = in.next();
+            int spend = in.nextInt();
+            try {
+                Class annotatedClass = FamilyMember.class;
+                Method[] methods = annotatedClass.getMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(FamilyBudget.class)) {
+                        FamilyBudget family = method
+                                .getAnnotation(FamilyBudget.class);
+                        String userRole = family.userRole();
+                        int budgetLimit = -1 /*TODO ~~Complete the interface~~*/;
+                        if (userRole.equals(role)) {
+                            if(false/*TODO ~~Complete the interface~~*/){
+                                method.invoke(FamilyMember.class.newInstance(),
+                                        budgetLimit, spend);
+                            }else{
+                                System.out.println("Budget Limit Over");
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            testCases--;
+        }
+    }
+}
+
+
+
